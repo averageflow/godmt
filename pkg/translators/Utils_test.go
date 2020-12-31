1ccdccdc0239fc8d4601bf8a3c272b46ba421e21
J. J. Bigorra <jjba@protonmail.ch>
2020-12-31 11:47:45 +0100

    add tests for record functionality

--- pkg/translators/Utils_test.go
+++ pkg/translators/Utils_test.go
@@ -27,3 +27,43 @@ func TestIsEmbeddedStructForInheritance(t *testing.T) {
 		t.Errorf("Expected sut to be an embedded struct for inheritance")
 	}
 }
+
+func TestTransformTypeScriptRecord(t *testing.T) {
+	testTable := map[string]string{
+		"map[string]string":           "Record<string, string>",
+		"map[string]int":              "Record<string, number>",
+		"map[int]string":              "Record<number, string>",
+		"map[string]interface{}":      "Record<string, any>",
+		"map[string]map[int]string":   "Record<string, Record<number, string>>",
+		"map[string][]map[int]string": "Record<string, Record<number, string>[]>",
+		"map[int][]map[string]int":    "Record<number, Record<string, number>[]>",
+		"map[string][]int":            "Record<string, number[]>",
+	}
+
+	for i := range testTable {
+		sut := TransformTypeScriptRecord(i)
+		if sut != testTable[i] {
+			t.Errorf("Expected %s, got %s", testTable[i], sut)
+		}
+	}
+}
+
+func TestTransformSwiftRecord(t *testing.T) {
+	testTable := map[string]string{
+		"map[string]string":           "Dictionary<String, String>",
+		"map[string]int":              "Dictionary<String, Int>",
+		"map[int]string":              "Dictionary<Int, String>",
+		"map[string]interface{}":      "Dictionary<String, Any>",
+		"map[string]map[int]string":   "Dictionary<String, Dictionary<Int, String>>",
+		"map[string][]map[int]string": "Dictionary<String, [Dictionary<Int, String>]>",
+		"map[int][]map[string]int":    "Dictionary<Int, [Dictionary<String, Int>]>",
+		"map[string][]int":            "Dictionary<String, [Int]>",
+	}
+
+	for i := range testTable {
+		sut := TransformSwiftRecord(i)
+		if sut != testTable[i] {
+			t.Errorf("Expected %s, got %s", testTable[i], sut)
+		}
+	}
+}
